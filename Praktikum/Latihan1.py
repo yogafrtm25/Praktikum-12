cf8d63ccb077057f51c3c92f0b2015f01d50d594
yogafrtm25 <yogafrtm25@gmail.com>
2022-12-28 19:30:54 +0700

    Menambakab File

---
+++ Praktikum/Latihan1.py
@@ -0,0 +1,27 @@
+print("Nama : Yoga Pratama")
+print("NIM  : 312210042")
+
+txt = 'Hello World'
+print()
+
+# Hitung jumlah karakternya
+print(len(txt))
+
+# Ambil karakter terakhir
+print(txt[-1])
+
+# Ambil karakter index ke-2 sampai index ke-4 (llo)
+print(txt[2:5])
+
+# Hilangkan spasi pada text tersebut (HelloWorld)
+print(txt.replace(" ", ""))
+
+# Ubah text menjadi huruf besar
+print(txt.upper())
+
+# Ubah text menjadi huruf kecil
+print(txt.lower())
+
+# Ganti karakter H dengan karakter J
+print(txt.replace("H", "J"))
+print()
---
+++ Praktikum/Latihan2.py
@@ -0,0 +1,7 @@
+print("Nama : Yoga Pratama")
+print("NIM  : 312210042")
+
+umur = 24
+txt = "\nHello, nama saya john, dan umur saya adalah {0} tahun\n"
+
+print(txt.format(umur))
